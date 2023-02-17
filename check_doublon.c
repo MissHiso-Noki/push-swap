c4d2b054efdada27fca584a79457439efe11b85e
Chloe <chloe.coste.pro@gmail.com>
2023-02-17 15:29:00 +0100

    avance dans la base

---
+++ check_doublon.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_doublon.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccoste <ccoste@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/17 13:27:45 by ccoste            #+#    #+#             */
+/*   Updated: 2023/02/17 15:14:35 by ccoste           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+#include "../utils/includes/libft.h"
+
+int     check_doublon(char *str, int size)
+{
+    // int i;
+    // int j;
+
+    // j = 1;
+    // i = 0;
+
+    // while(str[i] <= size)
+    // {
+    //     while (str[i] != str[j])
+    //     {
+    //         j++;
+    //     }
+    //     if (str[i] == str[j])
+    //     {
+    //         printf("Il y a un doublon");
+    //         return (0);
+    //     }
+    //     i++;
+    // }
+    // printf("Il n'y a pas de doublons");
+    // return (1);
+}
